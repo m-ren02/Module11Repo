4e15c0a48c950293d1a456b9e975beddf0d6ec22
m-ren02 <madirenckert@outlook.com>
2026-04-24 14:06:05 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-# Firstname Lastname
-# 11/15/2024
+# Madison Renckert
+# 04/24/2026
 # A simple Python program to print a message
 
+# Madison Renckert
+# 04/24/2026
+# added new message
+
 print("Hello, World!")
+
+print("Welcome to GitHub!"
